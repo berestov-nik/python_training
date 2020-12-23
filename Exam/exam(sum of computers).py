cd2643d52674589e1b41acdbe724231a0e597cb2
berestov-nik <berestov.fan@gmail.com>
2020-12-23 17:16:32 +0500

    created exam branch

---
+++ Exam/exam(sum of computers).py
@@ -0,0 +1,23 @@
+"""Стоимость покупки
+Напишите программу, которая считает стоимость трех компьютеров, состоящих из монитора, 
+системного блока, клавиатуры и мыши."""
+#Решение 1
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print((a + b + c + d) * 3)
+#Решение 2
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = a + b + c + d
+print(e * 3)
+#Решение 3
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = (a + b + c + d) * 3
+print(e)
